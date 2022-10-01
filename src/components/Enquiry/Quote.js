045749d10d7025038cfce396b8726333ae4ef1a8
Naveen VB <naveenvb560@gmail.com>
2022-10-01 18:27:08 +0530

    home page in progress

---
+++ src/components/Enquiry/Quote.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Quote.component.scss'
+
+const Quote=()=> {
+
+    return (
+        <div className="enquiry-container">
+            <p>Book the perfect Houseboat Cruise at Kumarakom at your own budget, we offer credible houseboat 
+                service at kumarakom with taste food,awesome sightseeing, and incredible service.</p>
+        </div>
+    )
+}
+
+export default Quote;
--- src/components/UI/Home/Home.js
+++ src/components/UI/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './Home.component.scss';
 import Contact from "../Contact";
+import Quote from "../../Enquiry/Quote";
 
 const Home =()=> {
 
     return (
         <div className="home-container">
             <Contact />
+            <Quote />
         </div>
     )
 }
